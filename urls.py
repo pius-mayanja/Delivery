818b992318d5f74c50adb25a4a2e364315b29795
Kats Brendan <katsbrendan@gmail.com>
2024-03-18 21:07:04 +0000

    Update urls.py

--- urls.py
+++ urls.py
@@ -4,3 +4,10 @@ from . import views
 urlpatterns = [
     path('', views.this, name="this"),  # Define your app-specific URLs here
 ]
+
+rom django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('main.urls')),  # Include the URLs from 'main.urls'
