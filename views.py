ccc72321405d3fdd0c7bf360b49bffcab6f8f36c
Kats Brendan <katsbrendan@gmail.com>
2024-03-18 21:26:22 +0000

    Update views.py

--- views.py
+++ views.py
@@ -1,9 +1,7 @@
 from django.shortcuts import render
-from django.template import loader
-from django.http import HttpResponse
 
 # Create your views here.
 
+
 def this(request):
-    template = loader.get_template('index.html')
-    return HttpResponse(template.reader())
+    return render(request, 'index.html')
