ac5079c2786c56f6d196905eb0ea26c4ea55577b
Mayanja Pius <mayanjapius72@gmail.com>
2024-08-09 21:54:44 +0300

    'updated checkout form'

--- Hivemind/orders/forms.py
+++ Hivemind/orders/forms.py
@@ -3,26 +3,14 @@ from .models import Order
 
 
 class OrderCreateForm(forms.ModelForm):   
-    first_name = forms.CharField(widget=forms.TextInput(attrs={
-        'placeholder': 'First Name (uppercass)',
-        'class': 'w-full py-2 px-3 rounded-xl border border-red-900'
-    })) 
-    last_name = forms.CharField(widget=forms.TextInput(attrs={
-        'placeholder': 'Last Name (uppercase)',
-        'class': 'w-full py-2 px-3 rounded-xl border border-red-900'
-    })) 
-
-    address = forms.CharField(widget=forms.TextInput(attrs={
-        'placeholder': 'Place of Residence',
-        'class': 'w-full py-2 px-3 rounded-xl border border-red-900 relative'
-    })) 
-    
-    phone_number = forms.CharField(widget=forms.TextInput(attrs={
-        'placeholder': 'Phone number eg. +256--',
-        'class': 'w-full py-2 px-3 rounded-xl border border-red-900'
-    })) 
-    
     class Meta:        
         model = Order        
         fields = ['first_name', 'last_name', 'address', 'phone_number']
+        widgets = {
+            'first_name': forms.TextInput(attrs={'placeholder': 'First Name (uppercass)', 'class': 'w-full py-2 px-3 rounded-xl border border-red-900'}),
+            'address': forms.TextInput(attrs={'id':'address','placeholder': 'Delivery address','class': 'w-full py-2 px-3 rounded-xl border border-red-900 autocomplete-container'}),
+            'last_name': forms.TextInput(attrs={'placeholder': 'Last Name (uppercase)','class': 'w-full py-2 px-3 rounded-xl border border-red-900'}),
+            'phone_number': forms.TextInput(attrs={'placeholder': 'Phone number eg. 256.....','class': 'w-full py-2 px-3 rounded-xl border border-red-900'}),
+    
+        }
 
--- Hivemind/orders/views.py
+++ Hivemind/orders/views.py
@@ -1,37 +1,28 @@
 from django.shortcuts import render,get_object_or_404, redirect
 from .models import OrderItem , Order
-from django.http import HttpResponseBadRequest,HttpResponse
+from django.http import HttpResponseBadRequest
 from .forms import OrderCreateForm
 from cart.cart import Cart
 from django.contrib.auth.decorators import login_required
 from user.decorators import customer_required
-import geocoder,folium
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
 
 @customer_required
 def order_create(request):  
     user = request.user
     if user.is_authenticated:
         cart = Cart(request)
-        # location = geocoder.osm('Nairobi')
-        # lat = location.lat
-        # lng = location.lng
-        # country = location.country
-        # m = folium.Map(location=[19, -12], zoom_start=2)
-        # folium.Marker([lat, lng], tooltip='Click for more',
-        #             popup=country).add_to(m)
-        # m = m._repr_html_()
-        
         if request.method == 'POST':        
             form = OrderCreateForm(request.POST)
-            if form.is_valid():     
+            if form.is_valid():
                 order = form.save(commit=False)
                 order.user = request.user  
                 order = form.save()            
                 for item in cart:                
                     OrderItem.objects.create(order=order, product=item['product'], price=item['price'],quantity=item['quantity'])        
                 cart.clear()
-            return render(request,'order/created.html',                          
-                        {'order': order})    
+            return render(request,'order/created.html', {'order': order})    
         else:        
             form = OrderCreateForm()    
         return render(request, 'jumia/checkout.html',{'cart': cart,'form': form,})
@@ -57,4 +48,6 @@ def order_details(request, id):
     orders = Order.objects.filter(id=id).filter(user=request.user).first()
     product = OrderItem.objects.filter(order=orders)
     return render(request, 'order/order_details.html', {'orders':orders, 'product':product})
+
+
 
