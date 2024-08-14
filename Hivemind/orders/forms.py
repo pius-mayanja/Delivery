b27431deee106046bdcf2029e87eeb6b3078e422
Mayanja Pius <mayanjapius72@gmail.com>
2024-08-14 10:29:11 +0300

    "....."

--- Hivemind/orders/forms.py
+++ Hivemind/orders/forms.py
@@ -7,10 +7,10 @@ class OrderCreateForm(forms.ModelForm):
         model = Order        
         fields = ['first_name', 'last_name', 'address', 'phone_number']
         widgets = {
-            'first_name': forms.TextInput(attrs={'placeholder': 'First Name (uppercass)', 'class': 'w-full py-2 px-3 rounded-xl border border-red-900'}),
-            'address': forms.TextInput(attrs={'id':'address','placeholder': 'Delivery address','class': 'w-full py-2 px-3 rounded-xl border border-red-900 autocomplete-container'}),
-            'last_name': forms.TextInput(attrs={'placeholder': 'Last Name (uppercase)','class': 'w-full py-2 px-3 rounded-xl border border-red-900'}),
-            'phone_number': forms.TextInput(attrs={'placeholder': 'Phone number eg. 256.....','class': 'w-full py-2 px-3 rounded-xl border border-red-900'}),
+            'first_name': forms.TextInput(attrs={'placeholder': 'First Name (uppercass)', 'class': 'w-full py-1 lg:py-2 px-3 rounded-xl border border-red-900'}),
+            'address': forms.TextInput(attrs={'id':'address','placeholder': 'Delivery address','class': 'w-full py-1 lg:py-2 px-3 rounded-xl border border-red-900 autocomplete-container'}),
+            'last_name': forms.TextInput(attrs={'placeholder': 'Last Name (uppercase)','class': 'w-full py-1 lg:py-2 px-3 rounded-xl border border-red-900'}),
+            'phone_number': forms.TextInput(attrs={'placeholder': 'Phone number eg. 256.....','class': 'w-full py-1 lg:py-22 px-3 rounded-xl border border-red-900'}),
     
         }
 
