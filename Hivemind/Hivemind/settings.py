ed8c558d93327604aa2fbabdd9840564df5e61af
Mayanja Pius <mayanjapius72@gmail.com>
2024-08-20 12:50:16 +0300

    'orders'

--- Hivemind/Hivemind/settings.py
+++ Hivemind/Hivemind/settings.py
@@ -142,7 +142,7 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles/')
 
 
 STATICFILES_DIR = [
@@ -154,7 +154,7 @@ STATICFILES_DIR = [
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 
 # Default primary key field type
