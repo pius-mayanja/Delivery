1f50b8a658fb2256706999a20eec4b822dc4cfad
Mayanja Pius <mayanjapius72@gmail.com>
2024-08-26 11:38:46 +0300

    "authentication"

--- Hivemind/user/urls.py
+++ Hivemind/user/urls.py
@@ -7,7 +7,7 @@ from .forms import LoginForm
 app_name = 'user'
 
 urlpatterns = [
-    path('signup/', SignUp, name='signup'),
+    path('signup/', CustomerSignUpView.as_view(), name='signup'),
     path("signup/business/",BusinessSignUpView.as_view(), name="reg"),
     path('login/',LoginView.as_view(), name='login'),
     path('logout/', Logout_view , name='logout'),
